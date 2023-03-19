4caf52034a015478c6f922719f09ccd20a80e381
Ganapathy77 <106886401+Ganapathy77@users.noreply.github.com>
2023-03-19 20:04:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 // complete the given function
 
 function palindrome(str){
-
+	let str = str.toLowerCase;
+	let str = str.replace(' ', '');
+	let i=0, j=str.length-1;
+	while(i<j){
+		if(str[i] !== str[j]){
+			return false;
+		}
+		i++;  
+		j--;
+	}
 }
 module.exports = palindrome
